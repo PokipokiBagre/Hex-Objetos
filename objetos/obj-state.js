4f73b44bb8c25e3f1d5cbc795eaa70c2bd9314c7
PokipokiBagre <amanojaku344@gmail.com>
2026-03-01 11:11:07 -0500

    Update obj-state.js

--- objetos/obj-state.js
+++ objetos/obj-state.js
@@ -1,9 +1,7 @@
 export let invGlobal = {}; export let objGlobal = {}; export let historial = []; 
 export let estadoUI = {
     jugadorControl: null, jugadorInv: null, filtroRar: 'Todos', filtroMat: 'Todos',
-    busquedaOP: "", busquedaCat: "", busquedaInv: "", 
-    logCopy: "", 
-    esAdmin: false
+    busquedaOP: "", busquedaCat: "", busquedaInv: "", logCopy: "", esAdmin: false
 };
 export function guardar() { 
     localStorage.setItem('hex_obj_v4', JSON.stringify({ inv: invGlobal, obj: objGlobal, his: historial })); 
