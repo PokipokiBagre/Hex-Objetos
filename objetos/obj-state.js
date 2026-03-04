311c1261f3b5523e0701458266d61c129ca46396
PokipokiBagre <amanojaku344@gmail.com>
2026-03-04 16:06:28 -0500

    Update obj-state.js

--- objetos/obj-state.js
+++ objetos/obj-state.js
@@ -2,6 +2,7 @@ export let invGlobal = {};
 export let objGlobal = {}; 
 export let historial = []; 
 export let estadoUI = {
+    vistaActual: 'grilla',
     jugadorControl: null, 
     jugadorInv: null, 
     filtroRar: 'Todos', 
@@ -11,9 +12,19 @@ export let estadoUI = {
     busquedaInv: "", 
     logCopy: "", 
     esAdmin: false,
-    cambiosSesion: {} 
+    cambiosSesion: {},
+    modoSincronizado: true, // Switch Auto a 10s
+    partyLoot: [], // Jugadores seleccionados en el OP
+    lootMult: 1, // Modificador (x1, x5, etc)
+    transOrigen: null, // Para transferencias
+    transDestino: null
 };
 
 export function guardar() { 
-    localStorage.setItem('hex_obj_v4', JSON.stringify({ inv: invGlobal, obj: objGlobal, his: historial })); 
+    localStorage.setItem('hex_obj_v4', JSON.stringify({ 
+        inv: invGlobal, 
+        obj: objGlobal, 
+        his: historial,
+        modoSync: estadoUI.modoSincronizado 
+    })); 
 }
