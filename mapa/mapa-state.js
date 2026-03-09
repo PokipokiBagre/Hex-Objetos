f00733f1b121d15e33a59783d39f51e61b71c1e0
PokipokiBagre <amanojaku344@gmail.com>
2026-03-09 16:14:38 -0500

    Update mapa-state.js

--- mapa/mapa-state.js
+++ mapa/mapa-state.js
@@ -1,7 +1,7 @@
 export const estadoMapa = {
-    esAdmin: false, // <--- NUEVO: Control de edición
+    esAdmin: false,
     nodos: [],
-    enlaces: [], 
+    enlaces: [],
     camara: { x: window.innerWidth/2, y: window.innerHeight/2, zoom: 0.8 },
     interaccion: {
         isDraggingBg: false,
@@ -10,14 +10,14 @@ export const estadoMapa = {
         lastMouseX: 0,
         lastMouseY: 0
     },
-    cambiosPendientes: false 
+    cambiosPendientes: false
 };
 
 export const COLOR_AFINIDAD = {
-    'Física': '#e2a673', 
-    'Energética': '#f3b67a', 
-    'Espiritual': '#7df0a7', 
-    'Mando': '#a4d3f2', 
-    'Psíquica': '#dcb1f0', 
+    'Física': '#e2a673',
+    'Energética': '#f3b67a',
+    'Espiritual': '#7df0a7',
+    'Mando': '#a4d3f2',
+    'Psíquica': '#dcb1f0',
     'Oscura': '#c285ff'
 };
