cae318b41a044fc1fc07731f2207385e3fee2829
PokipokiBagre <amanojaku344@gmail.com>
2026-03-09 20:59:55 -0500

    Update mapa-state.js

--- mapa/mapa-state.js
+++ mapa/mapa-state.js
@@ -2,6 +2,7 @@ export const estadoMapa = {
     esAdmin: false,
     nodos: [],
     enlaces: [],
+    math: { originX: 0, originY: 0, maxXDist: 2, maxYDist: 2 }, 
     camara: { x: window.innerWidth/2, y: window.innerHeight/2, zoom: 0.8 },
     interaccion: {
         isDraggingBg: false,
@@ -22,11 +23,11 @@ export const COLOR_AFINIDAD = {
     'Oscura': '#c285ff'
 };
 
-// ESTÉTICA ROLERA
+// ESTÉTICA DE ROL DEFINITIVA
 export const ESTETICA = {
-    lineaDescubierta: 'rgba(160, 120, 180, 0.35)', // Violeta grisáceo sutil y delgado (Reemplaza al blanco)
+    lineaDescubierta: 'rgba(140, 100, 160, 0.35)', // Violeta tenue/sutil (Reemplaza a la blanca)
     lineaMostaza: 'rgba(212, 175, 55, 0.8)',       // Mostaza para incompletos
     lineaRosa: 'rgba(200, 60, 100, 0.6)',          // Rosa para sellados
-    lineaPrecedente: 'rgba(138, 43, 226, 1)',      // Morado Violeta (Hover)
-    lineaSaliente: 'rgba(255, 236, 139, 1)'        // Amarillo Dorado (Hover)
+    lineaPrecedente: 'rgba(138, 43, 226, 1)',      // Morado Violeta (Llega al nodo)
+    lineaSaliente: 'rgba(255, 236, 139, 1)'        // Amarillo Dorado (Sale del nodo)
 };
