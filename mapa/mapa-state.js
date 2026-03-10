cd3ac4f8c14a09d03b5e5f87d6ceb98f91bd1d35
PokipokiBagre <amanojaku344@gmail.com>
2026-03-09 20:53:35 -0500

    Update mapa-state.js

--- mapa/mapa-state.js
+++ mapa/mapa-state.js
@@ -2,8 +2,6 @@ export const estadoMapa = {
     esAdmin: false,
     nodos: [],
     enlaces: [],
-    // SCALE absoluto: Evita que el mapa se comprima al alejar nodos
-    math: { originX: 0, originY: 0, scale: 3.5 }, 
     camara: { x: window.innerWidth/2, y: window.innerHeight/2, zoom: 0.8 },
     interaccion: {
         isDraggingBg: false,
@@ -24,11 +22,11 @@ export const COLOR_AFINIDAD = {
     'Oscura': '#c285ff'
 };
 
-// ESTÉTICA ROLERA MEJORADA
+// ESTÉTICA ROLERA
 export const ESTETICA = {
-    lineaDescubierta: 'rgba(130, 90, 160, 0.4)', // Violeta grisáceo muy tenue y delgado (EX-BLANCA)
-    lineaMostaza: 'rgba(212, 175, 55, 0.8)',     // Mostaza para incompletos
-    lineaRosa: 'rgba(200, 60, 100, 0.6)',        // Rosa para sellados
-    lineaPrecedente: 'rgba(138, 43, 226, 1)',    // Morado Violeta (Hover/Selección)
-    lineaSaliente: 'rgba(255, 236, 139, 1)'      // Amarillo Dorado (Hover/Selección)
+    lineaDescubierta: 'rgba(160, 120, 180, 0.35)', // Violeta grisáceo sutil y delgado (Reemplaza al blanco)
+    lineaMostaza: 'rgba(212, 175, 55, 0.8)',       // Mostaza para incompletos
+    lineaRosa: 'rgba(200, 60, 100, 0.6)',          // Rosa para sellados
+    lineaPrecedente: 'rgba(138, 43, 226, 1)',      // Morado Violeta (Hover)
+    lineaSaliente: 'rgba(255, 236, 139, 1)'        // Amarillo Dorado (Hover)
 };
