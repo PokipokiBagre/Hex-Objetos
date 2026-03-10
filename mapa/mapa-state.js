9b4993a3075cee1f6d0720f22aec022d483400b3
PokipokiBagre <amanojaku344@gmail.com>
2026-03-10 16:28:17 -0500

    Update mapa-state.js

--- mapa/mapa-state.js
+++ mapa/mapa-state.js
@@ -1,5 +1,6 @@
 export const estadoMapa = {
     esAdmin: false,
+    modoVisual: 'descubiertos', // Modo por defecto al cargar (Violeta/Dorado pastel global)
     jugadorActivo: 'Todos',
     jugadores: [],
     inventario: {},
@@ -35,19 +36,15 @@ export const ESTETICA = {
     lineaDescubierta: 'rgba(210, 190, 230, 0.2)', 
     lineaMostaza: 'rgba(212, 175, 55, 0.3)',       
     lineaRosa: 'rgba(200, 60, 100, 0.25)',         
-    
     lineaPrecedente: 'rgba(177, 156, 217, 0.5)',   
     lineaSaliente: 'rgba(236, 213, 154, 0.5)'     
 };
 
-// --- NUEVA SECCIÓN PARA VISTA PERSONAJE (COLORES SUAVIZADOS) ---
 export const COLORES_JUGADOR = {
     posesionMorada: 'rgba(150, 131, 200, 0.6)', 
-
     doradoInmediato: 'rgba(236, 213, 154, 0.7)', 
     doradoMedio: 'rgba(212, 196, 146, 0.5)',     
     doradoTenue: 'rgba(188, 180, 156, 0.4)',     
-    
     doradoRastreo: 'rgba(160, 150, 130, 0.3)',   
-    fondoNeutro: 'rgba(60, 60, 65, 0.25)'       
+    fondoNeutro: 'rgba(60, 60, 65, 0.25)'        
 };
